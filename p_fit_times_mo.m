02b5705b8f4a66c10eb58a56b560c57880c5cbb0
Chris Sherwood Win10 desktop <csherwood@usgs.gov>
2018-06-04 11:32:24 -0400

    added example profiles plot

--- p_fit_times_mo.m
+++ p_fit_times_mo.m
@@ -19,18 +19,16 @@ load suspsed_ba20_20rstrim_crs_cen
         'trandn','tranz','tranattn','Trans. Attn.';...
         'ADVdn','OBSz','OBS','OBS Backscatter';...
         'ADVdn','ADVz','ADVagc','ADV AGC';...
-        'absstc','absszc','abss1','ABSS 1';...
-        'absstc','absszc','abss2','ABSS 2';...
-        'absstc','absszc','abss3','ABSS 3';...
+        'absstc','absszc','abss1','1 MHz ABSS';...
+        'absstc','absszc','abss2','2.5 MHz ABSS';...
+        'absstc','absszc','abss3','4 MHz ABSS';...
         'UMEtc','UMEzc','UMEgamma','gamma';...
         };
 
 za = 0.1; % (m) Ca estimates standardized to this elevation
 nav = 0;  % number of profiles to left and right to be averaged
 %%
-ino = 2;
-
-ydt = 276.; % list of target year days
+ydt = 276.09; % list of target year days
 dnt = datenum(2011,1,1)+ydt-1
 datestr(dnt)
 
@@ -38,7 +36,8 @@ figure(1); clf
 % Cp650
 plotnum=0;
 for ino=[7]
-    plotnum = plotnum+1
+    disp('UME Cp605')
+    plotnum = plotnum+1;
     allt=ba.(inst{ino,1});
     allz=ba.(inst{ino,2});
     allc=ba.(inst{ino,3});
@@ -46,40 +45,77 @@ for ino=[7]
     if(ifnorm)
         allc = allc/(nanmean(allc(:)));
     end
-    if(ino==5) % convert LISST to mass (roughly)
-        % allc = (allc*(2.7/1.5)-1.3); % g/m3
-        allc = (allc*(2.7/1.5)); % g/m3
-    end
-    if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
-        % allc = (allc*2.7-1.3); %g/m3
-        allc = (allc*2.7); %g/m3
-    end
-    
-    if(ino==8) % no variance for Chl (why not?)
-        allvc=ones(size(allc))
-    else
-        allvc=vv.(inst{ino,3});
-    end
-    dz = diff(allz(1:2,1));
-    [ nz nt ]= size( allz )
-    
+%     if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
+%         % allc = (allc*2.7-1.3); %g/m3
+%         allc = (allc*2.7); %g/m3
+%     end
+
     % find target time
     i = find(allt(6,:)>dnt,1,'first');
-    
-    ic = 1;
     
     c = allc(:,i);
     varc = allvc(:,i);
     z = allz(:,i);
-    t = nanmean(allt(:,i))
+    t = nanmean(allt(:,i));
     tmin = nanmin(nanmin(allt(:,i)));
     tmax = nanmax(nanmax(allt(:,i)));
-    ok = (~isnan(c+z))
+    ok = (~isnan(c+z));
     
     if(sum(ok)>3)
         datestr(t)
-        datestr(tmin)
-        datestr(tmax)
+%         datestr(tmin)
+%         datestr(tmax)
+        subplot(4,3,plotnum)
+        pf = pfit( c(ok), z(ok),0, za);
+        loglog(c(ok)./pf.Ca,z(ok),'ok')
+        hold on
+        zest = logspace( log10(pf.za),log10(max(z(ok))), 20);
+        Cest = (zest./za).^pf.p;
+        loglog(Cest,zest,'--k','linewidth',2)
+        pfnl = pfit_nlp( c(ok), z(ok), varc(ok),0, za);
+        
+        % normalize the nl fit by the linear Ca
+        Cest_nl = pfnl.Ca./pf.Ca*(zest./za).^pfnl.p;
+        loglog(Cest_nl,zest,'--r','linewidth',2)
+        
+        %xlim( [.5 500] )
+        ylim( [.1 2.5] )
+        xlim( [.01, 1.5])
+        ylabel('Elevation (mab)')
+        xlabel('Attenuation ( m^{-1} )');
+        title(inst{ino,4});
+        ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
+            datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
+        ht=text(.015,.3,ts);
+        set(ht,'fontsize',9);
+    end
+end
+%% LISST attenuation
+plotnum=2;
+for ino=[5]
+    disp('LISST Attenuation')
+    plotnum=plotnum+1;
+    allt=ba.(inst{ino,1});
+    allz=ba.(inst{ino,2});
+    allc=ba.(inst{ino,3});
+    
+    if(ifnorm)
+        allc = allc/(nanmean(allc(:)));
+    end
+
+    % find target time
+    i = find(allt(6,:)>dnt,1,'first');
+
+    c = allc(:,i);
+    varc = allvc(:,i);
+    z = allz(:,i);
+    t = nanmean(allt(:,i));
+    tmin = nanmin(nanmin(allt(:,i)));
+    tmax = nanmax(nanmax(allt(:,i)));
+    ok = (~isnan(c+z));
+    
+    if(sum(ok)>3)
+        datestr(t)
         subplot(4,3,plotnum)
         pf = pfit( c(ok), z(ok),0, za);
         loglog(c(ok)./pf.Ca,z(ok),'ok')
@@ -91,27 +127,18 @@ for ino=[7]
         % normalize the nl fit by the linear Ca
         Cest_nl = pfnl.Ca./pf.Ca*(zest./za).^pfnl.p;
         loglog(Cest_nl,zest,'--r','linewidth',2)
-        
-        %xlim( [.5 500] )
+
         ylim( [.1 2.5] )
-        xlim( [.01, 1.2])
+        xlim( [.01, 1.5])
         ylabel('Elevation (mab)')
         xlabel('Attenuation ( m^{-1} )');
-        title(inst{ino,3});
+        title(inst{ino,4});
         ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
-            datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-        text(.015,.3,ts)
-        shg
-        pause(.2)
-        pf.i = i;
-        pf.t = t;
-        pfa(ic)=pf
-        pfanl(ic)=pfnl;
-        ic = ic+1;
+            datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2);
+        text(.015,.3,ts);
     end
 end
-
-% LISST Vol. conc - micro and macro flocs
+%% LISST Vol. conc - micro and macro flocs
 plotnum=3;
 for ino=[3:4]
     plotnum = plotnum+1;
@@ -122,28 +149,10 @@ for ino=[3:4]
     if(ifnorm)
         allc = allc/(nanmean(allc(:)));
     end
-    if(ino==5) % convert LISST to mass (roughly)
-        % allc = (allc*(2.7/1.5)-1.3); % g/m3
-        allc = (allc*(2.7/1.5)); % g/m3
-    end
-    if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
-        % allc = (allc*2.7-1.3); %g/m3
-        allc = (allc*2.7); %g/m3
-    end
-    
-    if(ino==8) % no variance for Chl (why not?)
-        allvc=ones(size(allc))
-    else
-        allvc=vv.(inst{ino,3});
-    end
-    dz = diff(allz(1:2,1));
-    [ nz nt ]= size( allz )
-    
+
     % find target time
     i = find(allt(6,:)>dnt,1,'first');
-    
-    ic = 1;
-    
+
     c = allc(:,i);
     varc = allvc(:,i);
     z = allz(:,i);
@@ -167,28 +176,20 @@ for ino=[3:4]
         % normalize the nl fit by the linear Ca
         Cest_nl = pfnl.Ca./pf.Ca*(zest./za).^pfnl.p;
         loglog(Cest_nl,zest,'--r','linewidth',2)
-        
-        %xlim( [.5 500] )
+
         ylim( [.1 2.5] )
-        xlim( [.01, 1.2])
+        xlim( [.01, 1.5])
         ylabel('Elevation (mab)')
-        xlabel('Attenuation ( m^{-1} )');
-        title(inst{ino,3});
+        xlabel('Volume Concentration (m^3/m^3)');
+        title(inst{ino,4});
         ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
             datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-        text(.015,.3,ts)
-        shg
-        pause(.2)
-        pf.i = i;
-        pf.t = t;
-        pfa(ic)=pf
-        pfanl(ic)=pfnl;
-        ic = ic+1;
+        ht=text(.015,.3,ts)
+        set(ht,'fontsize',9)
     end
 end
 
-% LISST Vol. conc total
-if(0)
+%% LISST Vol. conc total
 plotnum=5;
 for ino=[1]
     plotnum = plotnum+1;
@@ -199,30 +200,16 @@ for ino=[1]
     if(ifnorm)
         allc = allc/(nanmean(allc(:)));
     end
-    if(ino==5) % convert LISST to mass (roughly)
-        % allc = (allc*(2.7/1.5)-1.3); % g/m3
-        allc = (allc*(2.7/1.5)); % g/m3
-    end
-    if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
-        % allc = (allc*2.7-1.3); %g/m3
-        allc = (allc*2.7); %g/m3
-    end
-    if(ino==8) % no variance for Chl (why not?)
-        allvc=ones(size(allc))
-    else
-        allvc=vv.(inst{ino,3});
-    end
-    dz = diff(allz(1:2,1));
-    [ nz nt ]= size( allz )
     
     % find target time
     i = find(allt(6,:)>dnt,1,'first');
     
-    ic = 1;
-    
     c = allc(:,i);
+    c(c<1e-8)=NaN;
     varc = allvc(:,i);
     z = allz(:,i);
+    disp([z,c,varc])
+    
     t = nanmean(allt(:,i))
     tmin = nanmin(nanmin(allt(:,i)));
     tmax = nanmax(nanmax(allt(:,i)));
@@ -246,24 +233,18 @@ for ino=[1]
         
         %xlim( [.5 500] )
         ylim( [.1 2.5] )
-        xlim( [.01, 1.2])
+        xlim( [.01, 1.5])
         ylabel('Elevation (mab)')
-        xlabel('Attenuation ( m^{-1} )');
-        title(inst{ino,3});
+        xlabel('Vol. Concentration (m^3/m^3)');
+        title(inst{ino,4});
         ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
             datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-        text(.015,.3,ts)
-        shg
-        pause(.2)
-        pf.i = i;
-        pf.t = t;
-        pfa(ic)=pf
-        pfanl(ic)=pfnl;
-        ic = ic+1;
+        ht = text(.015,.3,ts)
+        set(ht,'fontsize',9)
     end
 end
-end
-% ABSS profiles
+
+%% ABSS profiles
 plotnum=6;
 for ino=[13:15]
     plotnum = plotnum+1
@@ -274,28 +255,10 @@ for ino=[13:15]
     if(ifnorm)
         allc = allc/(nanmean(allc(:)));
     end
-    if(ino==5) % convert LISST to mass (roughly)
-        % allc = (allc*(2.7/1.5)-1.3); % g/m3
-        allc = (allc*(2.7/1.5)); % g/m3
-    end
-    if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
-        % allc = (allc*2.7-1.3); %g/m3
-        allc = (allc*2.7); %g/m3
-    end
-    
-    if(ino==8) % no variance for Chl (why not?)
-        allvc=ones(size(allc))
-    else
-        allvc=vv.(inst{ino,3});
-    end
-    dz = diff(allz(1:2,1));
-    [ nz nt ]= size( allz )
     
     % find target time
     i = find(allt(6,:)>dnt,1,'first');
-    
-    ic = 1;
-    
+
     c = allc(:,i);
     varc = allvc(:,i);
     z = allz(:,i);
@@ -322,173 +285,20 @@ for ino=[13:15]
         
         %xlim( [.5 500] )
         ylim( [.1 2.5] )
-        xlim( [.01, 1.2])
+        xlim( [.01, 1.5])
         ylabel('Elevation (mab)')
-        xlabel('Attenuation ( m^{-1} )');
-        title(inst{ino,3});
+        xlabel('Backscatter Intensity (rel) )');
+        title(inst{ino,4});
         ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
             datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-        text(.015,.3,ts)
-        shg
-        pause(.2)
-        pf.i = i;
-        pf.t = t;
-        pfa(ic)=pf
-        pfanl(ic)=pfnl;
-        ic = ic+1;
+        ht=text(.015,.3,ts);
+        set(ht,'fontsize',9)
     end
 end
-% LISST Vol. conc
-plotnum=3;
-for ino=[2:4]
-    plotnum = plotnum+1;
-    allt=ba.(inst{ino,1});
-    allz=ba.(inst{ino,2});
-    allc=ba.(inst{ino,3});
-    
-    if(ifnorm)
-        allc = allc/(nanmean(allc(:)));
-    end
-    if(ino==5) % convert LISST to mass (roughly)
-        % allc = (allc*(2.7/1.5)-1.3); % g/m3
-        allc = (allc*(2.7/1.5)); % g/m3
-    end
-    if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
-        % allc = (allc*2.7-1.3); %g/m3
-        allc = (allc*2.7); %g/m3
-    end
-    
-    if(ino==8) % no variance for Chl (why not?)
-        allvc=ones(size(allc))
-    else
-        allvc=vv.(inst{ino,3});
-    end
-    dz = diff(allz(1:2,1));
-    [ nz nt ]= size( allz )
-    
-    % find target time
-    i = find(allt(6,:)>dnt,1,'first');
-    
-    ic = 1;
-    
-    c = allc(:,i);
-    varc = allvc(:,i);
-    z = allz(:,i);
-    t = nanmean(allt(:,i))
-    tmin = nanmin(nanmin(allt(:,i)));
-    tmax = nanmax(nanmax(allt(:,i)));
-    ok = (~isnan(c+z))
-    
-    if(sum(ok)>3)
-        datestr(t)
-        datestr(tmin)
-        datestr(tmax)
-        subplot(4,3,plotnum)
-        pf = pfit( c(ok), z(ok),0, za);
-        loglog(c(ok)./pf.Ca,z(ok),'ok')
-        hold on
-        zest = logspace( log10(pf.za),log10(max(z(ok))), 20);
-        Cest = (zest./za).^pf.p;
-        loglog(Cest,zest,'--k','linewidth',2)
-        pfnl = pfit_nlp( c(ok), z(ok), varc(ok),0, za);
-        % normalize the nl fit by the linear Ca
-        Cest_nl = pfnl.Ca./pf.Ca*(zest./za).^pfnl.p;
-        loglog(Cest_nl,zest,'--r','linewidth',2)
-        
-        %xlim( [.5 500] )
-        ylim( [.1 2.5] )
-        xlim( [.01, 1.2])
-        ylabel('Elevation (mab)')
-        xlabel('Attenuation ( m^{-1} )');
-        title(inst{ino,3});
-        ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
-            datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-        text(.015,.3,ts)
-        shg
-        pause(.2)
-        pf.i = i;
-        pf.t = t;
-        pfa(ic)=pf
-        pfanl(ic)=pfnl;
-        ic = ic+1;
-    end
-end
-% LISST attenuation
-plotnum=2;
-for ino=[5]
-    plotnum=plotnum+1;
-    allt=ba.(inst{ino,1});
-    allz=ba.(inst{ino,2});
-    allc=ba.(inst{ino,3});
-    
-    if(ifnorm)
-        allc = allc/(nanmean(allc(:)));
-    end
-    if(ino==5) % convert LISST to mass (roughly)
-        % allc = (allc*(2.7/1.5)-1.3); % g/m3
-        allc = (allc*(2.7/1.5)); % g/m3
-    end
-    if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
-        % allc = (allc*2.7-1.3); %g/m3
-        allc = (allc*2.7); %g/m3
-    end
-    
-    if(ino==8) % no variance for Chl (why not?)
-        allvc=ones(size(allc))
-    else
-        allvc=vv.(inst{ino,3});
-    end
-    dz = diff(allz(1:2,1));
-    [ nz nt ]= size( allz )
-    
-    % find target time
-    i = find(allt(6,:)>dnt,1,'first');
-    
-    ic = 1;
-    
-    c = allc(:,i);
-    varc = allvc(:,i);
-    z = allz(:,i);
-    t = nanmean(allt(:,i))
-    tmin = nanmin(nanmin(allt(:,i)));
-    tmax = nanmax(nanmax(allt(:,i)));
-    ok = (~isnan(c+z))
-    
-    if(sum(ok)>3)
-        datestr(t)
-        datestr(tmin)
-        datestr(tmax)
-        subplot(4,3,plotnum)
-        pf = pfit( c(ok), z(ok),0, za);
-        loglog(c(ok)./pf.Ca,z(ok),'ok')
-        hold on
-        zest = logspace( log10(pf.za),log10(max(z(ok))), 20);
-        Cest = (zest./za).^pf.p;
-        loglog(Cest,zest,'--k','linewidth',2)
-        pfnl = pfit_nlp( c(ok), z(ok), varc(ok),0, za);
-        % normalize the nl fit by the linear Ca
-        Cest_nl = pfnl.Ca./pf.Ca*(zest./za).^pfnl.p;
-        loglog(Cest_nl,zest,'--r','linewidth',2)
-        
-        %xlim( [.5 500] )
-        ylim( [.1 2.5] )
-        xlim( [.01, 1.2])
-        ylabel('Elevation (mab)')
-        xlabel('Attenuation ( m^{-1} )');
-        title(inst{ino,3});
-        ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
-            datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-        text(.015,.3,ts)
-        shg
-        pause(.2)
-        pf.i = i;
-        pf.t = t;
-        pfa(ic)=pf
-        pfanl(ic)=pfnl;
-        ic = ic+1;
-    end
-end
-% UME gamma
+
+
+
+%% UME gamma
 plotnum=9;
 for ino=[16]
     plotnum=plotnum+1;
@@ -499,14 +309,6 @@ for ino=[16]
     if(ifnorm)
         allc = allc/(nanmean(allc(:)));
     end
-    if(ino==5) % convert LISST to mass (roughly)
-        % allc = (allc*(2.7/1.5)-1.3); % g/m3
-        allc = (allc*(2.7/1.5)); % g/m3
-    end
-    if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
-        % allc = (allc*2.7-1.3); %g/m3
-        allc = (allc*2.7); %g/m3
-    end
     
     if(ino==8 || ino==16) % no variance for Chl or gamma
         allvc=ones(size(allc))
@@ -518,9 +320,7 @@ for ino=[16]
     
     % find target time
     i = find(allt(6,:)>dnt,1,'first');
-    
-    ic = 1;
-    
+
     c = allc(:,i);
     varc = allvc(:,i);
     z = allz(:,i);
@@ -550,20 +350,15 @@ for ino=[16]
         xlim( [.01, 1.2])
         ylabel('Elevation (mab)')
         xlabel('Attenuation ( m^{-1} )');
-        title(inst{ino,3});
+        title(inst{ino,4});
         ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
             datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-        text(.015,.3,ts)
-        shg
-        pause(.2)
-        pf.i = i;
-        pf.t = t;
-        pfa(ic)=pf
-        pfanl(ic)=pfnl;
-        ic = ic+1;
+        ht = text(.015,.3,ts);
+        set(ht,'fontsize',9)
     end
 end
-% UME chlorophyll
+
+%% UME chlorophyll
 plotnum=10;
 for ino=[8]
     plotnum=plotnum+1;
@@ -574,35 +369,22 @@ for ino=[8]
     if(ifnorm)
         allc = allc/(nanmean(allc(:)));
     end
-    if(ino==5) % convert LISST to mass (roughly)
-        % allc = (allc*(2.7/1.5)-1.3); % g/m3
-        allc = (allc*(2.7/1.5)); % g/m3
-    end
-    if(ino==7) % convert attn to Mass (forget offset...can lead to neg. Concs)
-        % allc = (allc*2.7-1.3); %g/m3
-        allc = (allc*2.7); %g/m3
-    end
-    
     if(ino==8 || ino==16) % no variance for Chl or gamma
         allvc=ones(size(allc))
     else
         allvc=vv.(inst{ino,3});
     end
-    dz = diff(allz(1:2,1));
-    [ nz nt ]= size( allz )
-    
+   
     % find target time
     i = find(allt(6,:)>dnt,1,'first');
-    
-    ic = 1;
-    
+
     c = allc(:,i);
     varc = allvc(:,i);
     z = allz(:,i);
     t = nanmean(allt(:,i))
     tmin = nanmin(nanmin(allt(:,i)));
     tmax = nanmax(nanmax(allt(:,i)));
-    ok = (~isnan(c+z))
+    ok = (~isnan(c+z));
     
     if(sum(ok)>3)
         datestr(t)
@@ -622,19 +404,65 @@ for ino=[8]
         
         %xlim( [.5 500] )
         ylim( [.1 2.5] )
-        xlim( [.01, 1.2])
+        xlim( [.01, 1.5])
         ylabel('Elevation (mab)')
         xlabel('Attenuation ( m^{-1} )');
-        title(inst{ino,3});
+        title(inst{ino,4});
         ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
             datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-        text(.015,.3,ts)
-        shg
-        pause(.2)
-        pf.i = i;
-        pf.t = t;
-        pfa(ic)=pf
-        pfanl(ic)=pfnl;
-        ic = ic+1;
+        ht=text(.015,.3,ts);
+        set(ht,'fontsize',9)
     end
 end
+%% LISST density - special case
+plotnum=11;
+for ino=[1]
+    plotnum = plotnum+1;
+    allt=ba.(inst{ino,1});
+    allz=ba.(inst{ino,2});
+    allc=ba.(inst{1,3})./ba.(inst{5,3});
+    
+    if(ifnorm)
+        allc = allc/(nanmean(allc(:)));
+    end
+    % find target time
+    i = find(allt(6,:)>dnt,1,'first');
+
+    c = allc(:,i);
+    varc = allvc(:,i);
+    z = allz(:,i);
+    t = nanmean(allt(:,i));
+    tmin = nanmin(nanmin(allt(:,i)));
+    tmax = nanmax(nanmax(allt(:,i)));
+    ok = (~isnan(c+z))
+    
+    if(sum(ok)>3)
+        datestr(t)
+        datestr(tmin)
+        datestr(tmax)
+        subplot(4,3,plotnum)
+        pf = pfit( c(ok), z(ok),0, za);
+        loglog(c(ok)./pf.Ca,z(ok),'ok')
+        hold on
+        zest = logspace( log10(pf.za),log10(max(z(ok))), 20);
+        Cest = (zest./za).^pf.p;
+        loglog(Cest,zest,'--k','linewidth',2);
+        pfnl = pfit_nlp( c(ok), z(ok), varc(ok),0, za);
+        % normalize the nl fit by the linear Ca
+        Cest_nl = pfnl.Ca./pf.Ca*(zest./za).^pfnl.p;
+        loglog(Cest_nl,zest,'--r','linewidth',2)
+        
+        %xlim( [.5 500] )
+        ylim( [.1 2.5] )
+        %xlim( [.01, 1.5])
+        ylabel('Elevation (mab)')
+        xlabel('LISST Density Proxy');
+        title('LISST Attenuation / Volume Conc.');
+        ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
+            datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
+        hs=text(.015,.3,ts);
+        set(hs,'fontsize',9);
+    end
+end
+%%
+print -dpng profiles.png
--- pfit_nlp.m
+++ pfit_nlp.m
@@ -1,62 +1,62 @@
 function pf=pfit_nlp(c,z,varc,iplot,za)
 % % Non-linear fit to Rouse profile
 % % Based on fit_lambda by Pat Dickhudt
-% 
-if(exist('iplot','var')~=1),iplot=0,end
+%
+if(exist('iplot','var')~=1),iplot=0;,end
 if(exist('za','var')~=1), za=z(1); end
-ok = (~isnan(c+z+varc)&c>0&z>0&varc>0) 
+ok = (~isnan(c+z+varc)&c>0&z>0&varc>0) ;
 y = c(ok);
 x = z(ok)./za;
 vary = varc(ok);
-disp([x y vary])
+disp([x y vary]);
 if(length(y)<3), error('Need at least 3 points in pfit_nl'), end
 
 % fit exponential function
 [efit,model] = expfit(x,y,[x(1) -4]);
 
-[sse, yhat] = model(efit)
+[sse, yhat] = model(efit);
 sst = sum( (yhat- mean(yhat) ).^2);
 r2 = 1-sse./sst;
 
 zest = logspace( log10(min(z)),log10(max(z)), 20);
 Cest = efit(1).*(zest./za).^efit(2);
 if(iplot)
-   hh=loglog(c,z,'ob');
-   set(hh,'color',[ .4 .4 .4])
-   hold on
-   for i=1:length(x)
-      hh = loglog([y(i)-sqrt(vary(i)) y(i)+sqrt(vary(i))],za*[x(i) x(i)],'-k')
-   set(hh,'color',[.4 .4 .4],'linewidth',2)
-   end
-   loglog(Cest,zest,'-r')
-   hh=loglog(y,za*x,'ob');
-   set(hh,'markerfacecolor',[1 .4 .4],'markersize',10)
+    hh=loglog(c,z,'ob');
+    set(hh,'color',[ .4 .4 .4])
+    hold on
+    for i=1:length(x)
+        hh = loglog([y(i)-sqrt(vary(i)) y(i)+sqrt(vary(i))],za*[x(i) x(i)],'-k')
+        set(hh,'color',[.4 .4 .4],'linewidth',2)
+    end
+    loglog(Cest,zest,'-r')
+    hh=loglog(y,za*x,'ob');
+    set(hh,'markerfacecolor',[1 .4 .4],'markersize',10)
 end
-pf.N = length(c)
+pf.N = length(c);
 pf.p = efit(2);
 pf.Ca = efit(1);
 pf.sa = NaN;
-pf.sb = NaN
+pf.sb = NaN;
 pf.r2 = r2;
 
-   function [estimates, model] = expfit(x,y,start_point)
-      % exponential fit function using fminsearch
-      % copied from Matlab help: Curve Fitting via Optimization
-      
-      % Call fminsearch with a random starting point.
-      %start_point = [.005 .0005];
-      model = @expfun;
-      options = optimset('TolFun',1E-6,'TolX',1E-6,'MaxFunEvals',1000,'MaxIter',1000);
-      [estimates,fval,exitflag] = fminsearch(model,start_point,options);
-      
-      % expfun accepts curve parameters as inputs, and outputs sse,
-      % the sum of squares error for
-      % and the FittedCurve. FMINSEARCH only needs sse, but we want
-      % to plot the FittedCurve at the end.
-      
-      function [sse, yhat] = expfun(params)
-         yhat = params(1) .* x.^params(2);
-         sse = sum( (yhat - y).^2 );
-      end
-   end
+    function [estimates, model] = expfit(x,y,start_point)
+        % exponential fit function using fminsearch
+        % copied from Matlab help: Curve Fitting via Optimization
+        
+        % Call fminsearch with a random starting point.
+        %start_point = [.005 .0005];
+        model = @expfun;
+        options = optimset('TolFun',1E-6,'TolX',1E-6,'MaxFunEvals',1000,'MaxIter',1000);
+        [estimates,fval,exitflag] = fminsearch(model,start_point,options);
+        
+        % expfun accepts curve parameters as inputs, and outputs sse,
+        % the sum of squares error for
+        % and the FittedCurve. FMINSEARCH only needs sse, but we want
+        % to plot the FittedCurve at the end.
+        
+        function [sse, yhat] = expfun(params)
+            yhat = params(1) .* x.^params(2);
+            sse = sum( (yhat - y).^2 );
+        end
+    end
 end
