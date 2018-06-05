0bf301dc159df69a6b110302715093d3161a82f7
Chris Sherwood on HP laptop <csherwood@usgs.gov>
2018-06-04 21:42:24 -0400

    minor progress

--- p_fit_times_mo.m
+++ p_fit_times_mo.m
@@ -96,7 +96,7 @@ for ii=1% 1:length(dnt)
    t = nanmean(allt(:,i));
    ok = (~isnan(c+z));
    
-   if(sum(ok)>3)     
+   if(sum(ok)>3)
       subplot(4,3,plotnum)
       plot_snippet
       xlabel('Attenuation ( m^{-1} )');
@@ -132,68 +132,51 @@ for ii=1% 1:length(dnt)
          %         datestr(tmin)
          %         datestr(tmax)
          subplot(4,3,plotnum)
-plot_snippet
+         plot_snippet
          xlabel('Volume Concentration (m^3/m^3)');
-      ttext = sprintf('%s w_s: %4.2f r^2: %4.2f',inst{ino,4},-1000*0.41*pfnl.p*ustar,pfnl.r2);
-      title(ttext)
+         ttext = sprintf('%s w_s: %4.2f r^2: %4.2f',inst{ino,4},-1000*0.41*pfnl.p*ustar,pfnl.r2);
+         title(ttext)
       end
    end
    
    %% LISST Vol. conc total
-   plotnum=5;
-   for ino=[1]
-      disp('LISST volume concentration')
-      plotnum = plotnum+1;
-      allt=ba.(inst{ino,1});
-      allz=ba.(inst{ino,2});
-      allc=ba.(inst{ino,3});
-      
-      if(ifnorm)
-         allc = allc/(nanmean(allc(:)));
-      end
-      
-      % find target time
-      i = find(allt(6,:)>dnt(ii),1,'first');
-      
-      c = allc(:,i);
-      varc = allvc(:,i);
-      z = allz(:,i);
-      disp([z,c,varc])
-      c(c<1e-8)=NaN;
-      
-      t = nanmean(allt(:,i));
-      tmin = nanmin(nanmin(allt(:,i)));
-      tmax = nanmax(nanmax(allt(:,i)));
-      ok = (~isnan(c+z));
-      
-      if(sum(ok)>3)
-         datestr(t)
-         %         datestr(tmin)
-         %         datestr(tmax)
-         subplot(4,3,plotnum)
-         pf = pfit( c(ok), z(ok),0, za);
-         loglog(c(ok)./pf.Ca,z(ok),'ok')
-         hold on
-         zest = logspace( log10(pf.za),log10(max(z(ok))), 20);
-         Cest = (zest./za).^pf.p;
-         loglog(Cest,zest,'--k','linewidth',2)
-         pfnl = pfit_nlp( c(ok), z(ok), varc(ok),0, za);
-         % normalize the nl fit by the linear Ca
-         Cest_nl = pfnl.Ca./pf.Ca*(zest./za).^pfnl.p;
-         loglog(Cest_nl,zest,'--r','linewidth',2)
-         
-         %xlim( [.5 500] )
-         ylim( [.1 2.5] )
-         xlim( [.01, 1.5])
-         ylabel('Elevation (mab)')
-         xlabel('Vol. Concentration (m^3/m^3)');
-         title(inst{ino,4});
-         ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
-            datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-         ht = text(.015,.3,ts);
-         set(ht,'fontsize',9);
-      end
+   plotnum=6;
+   ino=[1]
+   disp('LISST volume concentration')
+   allt=ba.(inst{ino,1});
+   allz=ba.(inst{ino,2});
+   allc=ba.(inst{ino,3})(:,i);
+   
+   if(ifnorm)
+      allc = allc/(nanmean(allc(:)));
    end
+   
+   % find target time
+   i = find(allt(6,:)>dnt(ii),1,'first');
+   
+   c = allc(:,i);
+   allvc=vv.(inst{ino,3});
+   varc = allvc(:,i);
+   z = allz(:,i);
+   disp([z,c,varc])
+   c(c<1e-8)=NaN;
+   
+   t = nanmean(allt(:,i));
+   tmin = nanmin(nanmin(allt(:,i)));
+   tmax = nanmax(nanmax(allt(:,i)));
+   ok = (~isnan(c+z));
+   
+   if(sum(ok)>3)
+      datestr(t)
+      %         datestr(tmin)
+      %         datestr(tmax)
+      subplot(4,3,plotnum)
+      plot_snippet
+      xlabel('Vol. Concentration (m^3/m^3)');
+      ttext = sprintf('%s w_s: %4.2f r^2: %4.2f',inst{ino,4},-1000*0.41*pfnl.p*ustar,pfnl.r2);
+      title(ttext)
+   end
+   
    
    %% ABSS profiles
    plotnum=6;
@@ -224,27 +207,9 @@ plot_snippet
          %         datestr(tmin)
          %         datestr(tmax)
          subplot(4,3,plotnum)
-         pf = pfit( c(ok), z(ok),0, za);
-         loglog(c(ok)./pf.Ca,z(ok),'ok')
-         hold on
-         zest = logspace( log10(pf.za),log10(max(z(ok))), 20);
-         Cest = (zest./za).^pf.p;
-         loglog(Cest,zest,'--k','linewidth',2)
-         pfnl = pfit_nlp( c(ok), z(ok), varc(ok),0, za);
-         % normalize the nl fit by the linear Ca
-         Cest_nl = pfnl.Ca./pf.Ca*(zest./za).^pfnl.p;
-         loglog(Cest_nl,zest,'--r','linewidth',2)
-         
-         %xlim( [.5 500] )
-         ylim( [.1 2.5] )
-         xlim( [.01, 1.5])
-         ylabel('Elevation (mab)')
+         plot_snippet
          xlabel('Backscatter Intensity (rel) )');
-         title(inst{ino,4});
-         ts = sprintf('%s\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\nCa_n=%7.2f\np_n=% 5.2f\nr_n^2=%06.4f',...
-            datestr(t,'dd-mmm-yyyy HHMM'),pf.N,pf.Ca,-pf.p,pf.r2,pfnl.Ca,-pfnl.p,pfnl.r2)
-         ht=text(.015,.3,ts);
-         set(ht,'fontsize',9);
+         
       end
    end
    %% UME gamma
@@ -364,41 +329,39 @@ plot_snippet
       end
    end
    %% LISST density - special case
-   plotnum=11;
-   for ino=[1]
-      disp('LISST density')
-      plotnum = plotnum+1;
-      allt=ba.(inst{ino,1});
-      allz=ba.(inst{ino,2});
-      allc=ba.(inst{1,3})./ba.(inst{5,3});
+   plotnum=12;
+   ino=[1]
+   disp('LISST density')
+   allt=ba.(inst{ino,1});
+   allz=ba.(inst{ino,2});
+   allc=ba.(inst{1,3})./ba.(inst{5,3});
+   
+   if(ifnorm)
+      allc = allc/(nanmean(allc(:)));
+   end
+   % find target time
+   i = find(allt(6,:)>dnt(ii),1,'first');
+   
+   c = allc(:,i);
+   allv = sqrt(vv.(inst{1,3}).^2+vv.(inst{5,3}).^2);
+   varc = allv(:,i);
+   
+   z = allz(:,i);
+   t = nanmean(allt(:,i));
+   tmin = nanmin(nanmin(allt(:,i)));
+   tmax = nanmax(nanmax(allt(:,i)));
+   ok = (~isnan(c+z));
+   disp([z c varc])
+   
+   if(sum(ok)>3)
+      datestr(t)
+      %         datestr(tmin)
+      %         datestr(tmax)
+      subplot(4,3,plotnum)
+      plot_snippet
+      xlabel('LISST Density Proxy');
+      title('LISST Attenuation / Volume Conc.');
       
-      if(ifnorm)
-         allc = allc/(nanmean(allc(:)));
-      end
-      % find target time
-      i = find(allt(6,:)>dnt(ii),1,'first');
-      
-      c = allc(:,i);
-      allv = sqrt(vv.(inst{1,3}).^2+vv.(inst{5,3}).^2);
-      varc = allv(:,i);
-      
-      z = allz(:,i);
-      t = nanmean(allt(:,i));
-      tmin = nanmin(nanmin(allt(:,i)));
-      tmax = nanmax(nanmax(allt(:,i)));
-      ok = (~isnan(c+z));
-      disp([z c varc])
-      
-      if(sum(ok)>3)
-         datestr(t)
-         %         datestr(tmin)
-         %         datestr(tmax)
-         subplot(4,3,plotnum)
-         plot_snippet
-         xlabel('LISST Density Proxy');
-         title('LISST Attenuation / Volume Conc.');
-         
-      end
    end
    %%
    shg
