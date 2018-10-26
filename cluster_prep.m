307b1005cb25cb4c51f932bde976f25ae53ad4c0
Chris Sherwood on HP laptop <csherwood@usgs.gov>
2018-10-26 19:16:12 -0400

    added most of the optics

--- cluster_prep.m
+++ cluster_prep.m
@@ -50,9 +50,16 @@ ba.UMEbs650(ineg)=1e-8;
 allt=ba.UMEtc;
 allz=ba.UMEzc;
 allc=ba.UMEattn650;
+alld=ba.UMEbs532;
+alle=ba.UMEbs650;
 t1 = allt(3:9,1:2031);
 z1 = allz(3:9,1:2031);
-c1 = allc(3:9,1:2031);
+c1 = allc(3:9,1:2031); %attn650
+c4 = alld(3:9,1:2031); %bs532
+c5 = alle(3:9,1:2031); %bs650
+
+allf = ba.UMEchl;
+c6= allf(3:9,1:2031); % Chl
 
 % LISST D50v
 allt=ba.LISSTtc;
@@ -61,6 +68,7 @@ allc=ba.LISSTD50v;
 t2 = allt(3:9,1:2031); 
 z2 = allz(3:9,1:2031);
 c2 = allc(3:9,1:2031);
+
 % abss
 allt=ba.absstc;
 allz=ba.absszc;
@@ -69,10 +77,16 @@ t3 = allt(3:9,1:2031);
 z3 = allz(3:9,1:2031);
 c3 = allc(3:9,1:2031);
 
-good_columns = find(~isnan(sum([t1;t2;t3;z1;z2;z3;c1;c2;c3])));
+good_columns = find(~isnan(sum([t1;t2;t3;z1;z2;z3;c1;c2;c3;c4;c5;c6])));
 c1 = c1(:,good_columns);
 c2 = c2(:,good_columns);
 c3 = c3(:,good_columns);
+c4 = c4(:,good_columns);
+c5 = c5(:,good_columns);
+c6 = c6(:,good_columns);
+c7 =  (650/532)*log(c4./c5); % gammabb
+
+
 % calculate mean depths and mean times
 t = mean([t1(:,good_columns);t2(:,good_columns);t3(:,good_columns)],1);
 z = mean([z1(:,good_columns),z2(:,good_columns),z3(:,good_columns)],2);
@@ -83,17 +97,32 @@ size(c2)
 size(c3)
 size(z)
 size(t)
-% standardize each array
+
+%% standardize each array
 c1mean = mean(c1(:));
 c2mean = mean(c2(:));
 c3mean = mean(c3(:));
+c4mean = mean(c4(:));
+c5mean = mean(c5(:));
+c6mean = mean(c6(:));
+c7mean = mean(c7(:));
+
 c1std = std(c1(:));
 c2std = std(c2(:));
 c3std = std(c3(:));
+c4std = std(c4(:));
+c5std = std(c5(:));
+c6std = std(c6(:));
+c7std = std(c7(:));
+
 
 c1s = (c1-c1mean)/c1std;
 c2s = (c2-c2mean)/c2std;
 c3s = (c3-c3mean)/c3std;
+c4s = (c4-c4mean)/c4std;
+c5s = (c5-c5mean)/c5std;
+c6s = (c6-c6mean)/c6std;
+c7s = (c7-c7mean)/c7std;
 
 save profiles.mat t tstd z zstd c1mean c1std c2mean c2std c3mean c3std c1s c2s c3s
 
@@ -101,12 +130,10 @@ save profiles.mat t tstd z zstd c1mean c1std c2mean c2std c3mean c3std c1s c2s c
 uwi = interp1(dus,us_wave,t);
 usi = interp1(dus,ewus,t);
 
-
+%% cp605, d50, abss
 c123s = [c1s;c2s;c3s]';
-
 nk = 5
 idx = kmeans(c123s,nk);
-%%
 figure(1); clf
 ip=1
 for k=1:nk
@@ -118,14 +145,57 @@ for k=1:nk
       plot( mean(c2(:,find(idx==k)),2),z, '-')
       hold on
       xlim([145 320])
-
    subplot(nk,3,ip+2)
       plot( 1000*mean(c3(:,find(idx==k)),2),z, '-')
       xlim([.5 5])
    ip=ip+3
 end
-%%
+%
 figure(2)
 scatter(t,uwi,12,idx)
+%
+figure(3)
+scatter(usi,uwi,12,idx)
+%% all optics
+c12456s = [c1s;c2s;c4s;c5s;c6s]';
+nk = 6
+idx = kmeans(c12456s,nk);
+%
+figure(1); clf
+ip=1
+for k=1:nk
+   subplot(nk,5,ip)
+      plot( mean(c1(:,find(idx==k)),2),z, '-')
+      hold on
+      xlim([0 12])
+      if(k==1),title('cp650'),end
+   subplot(nk,5,ip+1)
+      plot( mean(c2(:,find(idx==k)),2),z, '-')
+      hold on
+      xlim([145 320])
+      if(k==1),title('LISST d50'),end
+   subplot(nk,5,ip+2)
+      plot( mean(c4(:,find(idx==k)),2),z, '-')
+      xlim([0 .3])
+      if(k==1),title('bs 532'),end
+
+    subplot(nk,5,ip+3)
+      plot( mean(c5(:,find(idx==k)),2),z, '-')
+      hold on
+      xlim([0 .3])
+            if(k==1),title('bs 650'),end
+
+   subplot(nk,5,ip+4)
+      plot( mean(c6(:,find(idx==k)),2),z, '-')
+      hold on
+      xlim([0 20])
+            if(k==1),title('chl'),end
+
+   ip=ip+5
+end
+%
+figure(2)
+scatter(t,uwi,12,idx)
+%
 figure(3)
 scatter(usi,uwi,12,idx)
